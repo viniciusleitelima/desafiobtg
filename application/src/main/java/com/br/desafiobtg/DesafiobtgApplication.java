5c9d8d0c3eaefbb58f700472742b820462ab0409
viniciuslima <viniciuslima@finchsolucoes.com.br>
2023-04-18 23:42:47 -0300

    inclusao de scan base package

--- application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
+++ application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableRabbit
 @SpringBootApplication
-@ComponentScan("com.br.desafiobtg.delivery")
-@ComponentScan("com.br.desafiobtg.domain")
-@ComponentScan("com.br.desafiobtg.integration")
+@ComponentScan("com.br.desafiobtg")
 public class DesafiobtgApplication {
 
 	public static void main(String[] args) {
