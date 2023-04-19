566bd59f60345526f9ae4b5597e814dfc20d2a5e
Vinicius Leite Lima <51835363+viniciusleitelima@users.noreply.github.com>
2023-04-18 23:26:06 -0300

    Update DesafiobtgApplication.java

--- application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
+++ application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableRabbit
-@SpringBootApplication(scanBasePackages = "desafiobtg")
+@SpringBootApplication(scanBasePackages = "com.br.desafiobtg")
 public class DesafiobtgApplication {
 
 	public static void main(String[] args) {
