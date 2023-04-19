c12d2acc639ef26dae5b4373294777c091943e17
viniciuslima <viniciuslima@finchsolucoes.com.br>
2023-04-18 23:34:12 -0300

    inclusao de scan base package

--- application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
+++ application/src/main/java/com/br/desafiobtg/DesafiobtgApplication.java
@@ -4,9 +4,13 @@ package com.br.desafiobtg;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableRabbit
-@SpringBootApplication(scanBasePackages = "desafiobtg")
+@SpringBootApplication
+@ComponentScan("com.br.desafiobtg.delivery")
+@ComponentScan("com.br.desafiobtg.domain")
+@ComponentScan("com.br.desafiobtg.integration")
 public class DesafiobtgApplication {
 
 	public static void main(String[] args) {
